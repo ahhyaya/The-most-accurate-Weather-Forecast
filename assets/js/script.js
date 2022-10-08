7eb9cf6f23647d95de25b5cf2239caf1b6fa2fa5
doldol123 <toruig59@gmail.com>
2022-10-07 23:46:41 -0700

    fix search btn and major city btn function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,13 @@ var repoSearchTerm = $("repo-search-term");
 //   });
 
 
+// search btn
 var formSearchHandler = function (event) {
     event.preventDefault();
-
-    var cityInput = city.value.trim();
-
+    var cityInput = city.val();
+    console.log(cityInput)
     if (cityInput) {
         getCityRepos(cityInput);
-
         currWeatherForcastEl.textContent = '';
         city.value = '';
     } else {
@@ -29,24 +28,24 @@ var formSearchHandler = function (event) {
     }
 };
 
+
+//majorcity btn
 var cityClickHandler = function (event) {
     var cityList = event.target.getAttribute('data-city');
-
     if (cityList) {
         getWeatherRepos(cityList);
-
         currWeatherForcastEl.textContent = '';
     }
 };
 
-
+// searcing city function
 var getCityRepos = function (city) {
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     fetch(queryURL)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayRepos(data, name); //cityname
+                    displayRepos(data.name, city); //cityname
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -57,55 +56,62 @@ var getCityRepos = function (city) {
         });
 }
 
+
+
+// get weather forcast function
 var getWeatherRepos = function (cityList) {
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q="  + cityList + "&appid=" + APIKey;
-  
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityList + "&appid=" + APIKey;
+
     fetch(queryURL).then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data.weather, cityList); //ck data source
-        });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.main.temp, cityList); //ck data source
+            });
+        } else {
+            alert('Error: ' + response.statusText);
+        }
     });
-  };
+};
 
-
-
-var displayRepos = function (repos, searchTerm) {
+//display function
+var displayCurrForcast = function (repos, searchTerm) {
     if (repos.length === 0) {
         currWeatherForcastEl.textContent = 'No repositories found.';
-      return;
+        return;
     }
-  
+
     repoSearchTerm.textContent = searchTerm;
-  
-    for (var i = 0; i < repos.length; i++) {
-      var repoName = repos[i].owner.login + '/' + repos[i].name;
-      
-      // We create those URLs when we write the repos to the page in the displayRepos() function, as shown in the following code:
-      var repoEl = document.createElement('a');
-      repoEl.classList = 'list-item flex-row justify-space-between align-center';
-      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-  
-      var titleEl = document.createElement('span');
-      titleEl.textContent = repoName;
-  
-      repoEl.appendChild(titleEl);
-  
-      var statusEl = document.createElement('span');
-      statusEl.classList = 'flex-row align-center';
-  
-      if (repos[i].open_issues_count > 0) {
-        statusEl.innerHTML =
-          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-      } else {
-        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-      }
-  
-      repoEl.appendChild(statusEl);
-  
-      repoContainerEl.appendChild(repoEl);
-    }
-  };
+    var repoCity = repos.name;
+    var repoTemp = repos.main.temp;
+    var repoWind = repos.wind.speed;
+    var repoHumidity = repos.main.humidity;
+    // var titleEl = document.createElement('span');
+    var cityDisplay = $("#city-date-icon");
+    cityDisplay.textContent = repoCity;
+    //  + repoTemp + repoWind + repoHumidity;
+    repoSearchTerm.appendChild(cityDisplay);
+
+}
+
+// var displayRepos = function (repos, searchTerm) {
+//     if (repos.length === 0) {
+//         currWeatherForcastEl.textContent = 'No repositories found.';
+//         return;
+//     }
+
+//     repoSearchTerm.textContent = searchTerm;
+
+//     for (var i = 0; i < repos.length; i++) {
+//         var repoCity = repos[i].name;
+//         var repoTemp = repos[i].main.temp;
+//         var repoWind = repos[i].wind.speed;
+//         var repoHumidity = repos[i].main.humidity;
+
+//         var titleEl = document.createElement('span');
+//         titleEl.textContent = repoCity + repoTemp + repoWind + repoHumidity;
+//          currWeatherForcastEl.appendChild(repoEl);
+//     }
+// };
+
+searchBtn.on('click', formSearchHandler);
+$(".majorBtn").on('click', cityClickHandler);
