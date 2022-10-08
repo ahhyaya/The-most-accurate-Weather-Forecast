c2de66b51e0df555eeb3625f87c60dfb241736b9
doldol123 <toruig59@gmail.com>
2022-10-07 22:16:52 -0700

    add currForcast to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,20 +3,40 @@ var APIKey = "6b4dd594eb93d97558f4b8bf3d0ad157";
 var city = $("#city-input");
 var currWeatherForcastEl = $("#curr-weather-forcast");
 
+// fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=6b4dd594eb93d97558f4b8bf3d0ad157')
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log('Weather: Raw data \n----------');
+//     console.log(data);
+//   });
+
+
 var formSearchHandler = function (event) {
     event.preventDefault();
-  
+
     var cityInput = city.value.trim();
-  
+
     if (cityInput) {
-      getCityRepos(cityInput);
-  
-      currWeatherForcastEl.textContent = '';
-      city.value = '';
+        getCityRepos(cityInput);
+
+        currWeatherForcastEl.textContent = '';
+        city.value = '';
     } else {
-      alert('Please enter a valid city name');
+        alert('Please enter a valid city name');
     }
-  };
+};
+
+var cityClickHandler = function (event) {
+    var cityList = event.target.getAttribute('data-city');
+
+    if (cityList) {
+        getWeatherRepos(cityList);
+
+        currWeatherForcastEl.textContent = '';
+    }
+};
 
 
 var getCityRepos = function (city) {
@@ -25,24 +45,27 @@ var getCityRepos = function (city) {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    displayRepos(data, city);
+                    displayRepos(data, name); //cityname
                 });
             } else {
                 alert('Error: ' + response.statusText);
             }
         })
         .catch(function (error) {
-            alert('Unable to connect to GitHub');
+            alert('Unable to connect to Weather Data Documentation');
         });
 }
 
-
-var cityClickHandler = function (event) {
-    var cityList = event.target.getAttribute('data-city');
+var getWeatherRepos = function (cityList) {
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q="  + cityList + "&appid=" + APIKey;
   
-    if (cityList) {
-      getFeaturedRepos(cityList);
-  
-      currWeatherForcastEl.textContent = '';
-    }
+    fetch(queryURL).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data.weather, cityList); //ck data source
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    });
   };
