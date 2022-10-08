000317c0a2c89ebc8e3fd011e7c11d53559a5d4b
doldol123 <toruig59@gmail.com>
2022-10-07 22:30:06 -0700

    add basic structure to display current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ var searchBtn = $("#search-btn");
 var APIKey = "6b4dd594eb93d97558f4b8bf3d0ad157";
 var city = $("#city-input");
 var currWeatherForcastEl = $("#curr-weather-forcast");
+var repoSearchTerm = $("repo-search-term");
 
 // fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid=6b4dd594eb93d97558f4b8bf3d0ad157')
 //   .then(function (response) {
@@ -68,4 +69,43 @@ var getWeatherRepos = function (cityList) {
         alert('Error: ' + response.statusText);
       }
     });
+  };
+
+
+
+var displayRepos = function (repos, searchTerm) {
+    if (repos.length === 0) {
+        currWeatherForcastEl.textContent = 'No repositories found.';
+      return;
+    }
+  
+    repoSearchTerm.textContent = searchTerm;
+  
+    for (var i = 0; i < repos.length; i++) {
+      var repoName = repos[i].owner.login + '/' + repos[i].name;
+      
+      // We create those URLs when we write the repos to the page in the displayRepos() function, as shown in the following code:
+      var repoEl = document.createElement('a');
+      repoEl.classList = 'list-item flex-row justify-space-between align-center';
+      repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
+  
+      var titleEl = document.createElement('span');
+      titleEl.textContent = repoName;
+  
+      repoEl.appendChild(titleEl);
+  
+      var statusEl = document.createElement('span');
+      statusEl.classList = 'flex-row align-center';
+  
+      if (repos[i].open_issues_count > 0) {
+        statusEl.innerHTML =
+          "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+      } else {
+        statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      }
+  
+      repoEl.appendChild(statusEl);
+  
+      repoContainerEl.appendChild(repoEl);
+    }
   };
