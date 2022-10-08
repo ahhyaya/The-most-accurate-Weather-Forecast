8efe0d2d9853de339e5aa0dfc9c0498fd498bbc8
doldol123 <toruig59@gmail.com>
2022-10-07 21:12:32 -0700

    add weather api with city var

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var searchBtn = $("#search-btn");
+var city;
+
+
+
+
+var getCityRepos = function (city) {
+    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid={6b4dd594eb93d97558f4b8bf3d0ad157}";
+}
+
+function searchByInput() {
+
+}
+
+
+searchBtn.on("click",searchByInput());
